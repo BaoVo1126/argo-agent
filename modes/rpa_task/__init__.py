5a4acc6e312d2298db9ed2477d0a4a02906b3028
Quoc Bao <voquocbao112006@gmail.com>
2026-09-08 15:13:56 +0700

    Update __init__.py

--- modes/rpa_task/__init__.py
+++ modes/rpa_task/__init__.py
@@ -1,5 +1,3 @@
-"""RPA task mode: run a goal, then assert the final page state."""
-
 from modes.rpa_task.runner import Check, TaskOutcome, TaskSpec, run_task
 
 __all__ = ["Check", "TaskOutcome", "TaskSpec", "run_task"]
