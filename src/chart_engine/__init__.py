ecc439c1f0d43a29c7e1d1207e494a152664bf84
Quoc Bao <voquocbao112006@gmail.com>
2026-09-08 15:01:23 +0700

    Update __init__.py

--- src/chart_engine/__init__.py
+++ src/chart_engine/__init__.py
@@ -1,5 +1,3 @@
-"""Rule-based chart selection and local matplotlib/seaborn rendering."""
-
 from src.chart_engine.rules import ChartSpec, ChartType, Kind, choose, profile
 from src.chart_engine.render import render
 
